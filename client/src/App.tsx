ed9cee2eea9410ce5ec185d3b6d06b2a3c3450f0
Roland Saavedra <rolandsaavedra22@gmail.com>
2025-06-20 23:57:32 -0700

    add link to resume builder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GetStarted from "./pages/GetStarted";
+import ResumeBuilderStep1 from "./pages/resume-builder/ResumeBuilderStep1";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/get-started" element={<GetStarted />} />
+          <Route path="/resume-builder" element={<ResumeBuilderStep1 />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
